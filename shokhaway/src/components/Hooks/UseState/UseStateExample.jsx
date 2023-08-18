caa801c61b01a5d36a69afb38949b08918ab62bf
shokhaway01 <shoxauey@gmail.com>
2023-08-18 17:55:26 +0500

    second project updating

---
+++ shokhaway/src/components/Hooks/UseState/UseStateExample.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const UseStateExample = () => {
+    const [numbers ,setNumbers] = React.useState([1])
+
+    const addNumber = () => {
+        const randNumber =
+    }
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default UseStateExample;
